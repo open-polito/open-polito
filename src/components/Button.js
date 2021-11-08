e91054307cd0e8ab4fac717ba7178690a27dfd0b
Robert Olaru <77898084+robertolaru@users.noreply.github.com>
2021-11-08 14:57:02 +0100

    Custom components; styles and colors files

---
+++ src/components/Button.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import {View, Pressable, StyleSheet} from 'react-native';
+import colors from '../colors';
+import {TextN} from './Text';
+
+export default function Button({text}) {
+  return (
+    <View>
+      <Pressable style={_styles.btn} android_ripple={{color: '#eee'}}>
+        <TextN text={text} weight="medium" color="white" />
+      </Pressable>
+    </View>
+  );
+}
+
+const _styles = StyleSheet.create({
+  btn: {
+    alignSelf: 'flex-end',
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: colors.gradient1,
+    paddingHorizontal: 24,
+    paddingVertical: 16,
+    borderRadius: 4,
+    elevation: 3,
+  },
+});
