e2d3dbdbed379632d162be8dbeef0cc5b346625f
Robert Olaru <77898084+robertolaru@users.noreply.github.com>
2021-11-08 23:51:47 +0100

    Removed useless test function

--- App.js
+++ App.js
@@ -1,20 +1,9 @@
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import React from 'react';
-import {View} from 'react-native';
 import LoginScreen from './src/screens/LoginScreen';
 
 const Stack = createNativeStackNavigator();
-
-function Test() {
-  return (
-    <View style={{flex: 1}}>
-      <View style={{flex: 1, backgroundColor: 'white'}}></View>
-      <View style={{flex: 1, backgroundColor: 'black'}}></View>
-      <View style={{flex: 1, backgroundColor: 'magenta'}}></View>
-    </View>
-  );
-}
 
 export default function App() {
   return (
