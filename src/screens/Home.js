2280e9aa03fc29f5493ca3d11a39b5bc902d5d79
Robert Olaru <77898084+robertolaru@users.noreply.github.com>
2021-11-09 22:07:53 +0100

    Basic authentication

---
+++ src/screens/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {View} from 'react-native';
+import {TextTitle} from '../components/Text';
+
+export default function Home(props) {
+  // Basic prototype to show authentication success
+  // TODO actual UI
+  return (
+    <View>
+      <TextTitle text="Home" />
+    </View>
+  );
+}
