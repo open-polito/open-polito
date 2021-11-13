608f931686edcb7fd15237b1f29ea5efc8e2a9e8
Robert Olaru <77898084+robertolaru@users.noreply.github.com>
2021-11-13 13:52:41 +0100

    Settings screen base

--- src/screens/Settings.js
+++ src/screens/Settings.js
@@ -1,6 +1,32 @@
 import React from 'react';
-import {View} from 'react-native';
+import {SafeAreaView, StatusBar, View} from 'react-native';
+import {ScrollView} from 'react-native-gesture-handler';
+import {useSelector} from 'react-redux';
+import colors from '../colors';
+import Header from '../components/Header';
+import styles from '../styles';
 
 export default function Settings() {
-  return <View></View>;
+  const {windowHeight} = useSelector(state => state.ui);
+
+  return (
+    <SafeAreaView style={{height: windowHeight - styles.tabNavigator.height}}>
+      <StatusBar
+        translucent
+        backgroundColor="transparent"
+        barStyle="dark-content"
+      />
+      <View
+        style={{
+          ...styles.container,
+          ...styles.safePaddingTop,
+          ...styles.withHorizontalPadding,
+          backgroundColor: colors.white,
+          height: windowHeight,
+        }}>
+        <Header text="Settings" noMarginBottom={true} />
+        <ScrollView></ScrollView>
+      </View>
+    </SafeAreaView>
+  );
 }
