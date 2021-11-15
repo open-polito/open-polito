7aed6b42f81930512eb62b7ebba54776322c2065
Robert Olaru <77898084+robertolaru@users.noreply.github.com>
2021-11-15 18:54:51 +0100

    Unnecessary imports

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -1,9 +1,8 @@
-import React, {useEffect, useState} from 'react';
+import React, {useState} from 'react';
 import {
   View,
   StatusBar,
   StyleSheet,
-  Keyboard,
   Dimensions,
   ImageBackground,
 } from 'react-native';
