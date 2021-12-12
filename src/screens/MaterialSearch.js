69ff314ac7a2ffc5c32811e53ecb4bce4c5c875d
Robert Olaru <77898084+robertolaru@users.noreply.github.com>
2021-12-12 23:02:56 +0100

    Wrap all screens in ScreenContainer to reduce code repetition

--- src/screens/MaterialSearch.js
+++ src/screens/MaterialSearch.js
@@ -1,12 +1,5 @@
 import React, {useEffect, useRef, useState} from 'react';
-import {
-  Pressable,
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  TextInput,
-  View,
-} from 'react-native';
+import {Pressable, ScrollView, TextInput, View} from 'react-native';
 import colors from '../colors';
 import styles from '../styles';
 import {useTranslation} from 'react-i18next';
@@ -15,6 +8,7 @@ import {useSelector} from 'react-redux';
 import {getMaterialList} from '../utils/material';
 import DirectoryItem from '../components/DirectoryItem';
 import {TextS, TextSubTitle} from '../components/Text';
+import ScreenContainer from '../components/ScreenContainer';
 export default function MaterialSearch({navigation}) {
   const {t} = useTranslation();
   const [query, setQuery] = useState(null);
@@ -46,96 +40,79 @@ export default function MaterialSearch({navigation}) {
   }
 
   return (
-    <SafeAreaView style={{height: '100%'}}>
-      <StatusBar
-        translucent
-        backgroundColor="transparent"
-        barStyle="dark-content"
-      />
+    <ScreenContainer>
       <View
         style={{
-          ...styles.container,
-          ...styles.safePaddingTop,
-          ...styles.withHorizontalPadding,
-          backgroundColor: colors.white,
+          flexDirection: 'row',
+          alignItems: 'center',
         }}>
-        <View
-          style={{
-            flexDirection: 'row',
-            alignItems: 'center',
+        <Pressable
+          onPress={() => {
+            navigation.goBack();
           }}>
-          <Pressable
-            onPress={() => {
-              navigation.goBack();
-            }}>
-            <Icon name="arrow-back" color={colors.black} size={32} />
-          </Pressable>
-          <TextInput
-            autoFocus={true}
-            ref={inputRef}
-            placeholder={t('search')}
-            placeholderTextColor={colors.mediumGray}
-            style={{
-              fontFamily: 'Montserrat-Bold',
-              ...styles.textExtraLarge,
-              ...styles.withHorizontalPadding,
-              width: '100%',
-              color: colors.black,
-            }}
-            onChangeText={txt => {
-              setQuery(txt.toLowerCase().trim());
-            }}
-            onSubmitEditing={searchMaterial}
-          />
-        </View>
-        {results != null && (
-          <TextSubTitle text={t('searchResults', {count: results.length})} />
-        )}
-        <View
+          <Icon name="arrow-back" color={colors.black} size={32} />
+        </Pressable>
+        <TextInput
+          autoFocus={true}
+          ref={inputRef}
+          placeholder={t('search')}
+          placeholderTextColor={colors.mediumGray}
           style={{
-            flexDirection: 'column',
-          }}>
-          {results != null &&
-            (results.length > 0 ? (
-              <ScrollView>
-                {results.map(item => {
-                  return (
-                    <View key={item.code}>
-                      <DirectoryItem
-                        tipo="file"
-                        key={item.code}
-                        nome={item.nome}
-                        filename={item.filename}
-                        data_inserimento={item.data_inserimento}
-                        size_kb={item.size_kb}
-                        code={item.code}
-                        corso={item.corso}
-                      />
-                    </View>
-                  );
-                })}
-                <View style={{marginBottom: 120}}></View>
-              </ScrollView>
-            ) : (
-              <View
-                style={{
-                  flexDirection: 'column',
-                  justifyContent: 'center',
-                  alignItems: 'center',
-                  width: '100%',
-                  height: '100%',
-                  marginTop: -styles.textExtraLarge.fontSize,
-                }}>
-                <Icon name="search-off" color={colors.gray} size={64} />
-                <TextS
-                  text={t('noResults')}
-                  weight="bold"
-                  color={colors.gray}
-                />
-              </View>
-            ))}
-        </View>
+            fontFamily: 'Montserrat-Bold',
+            ...styles.textExtraLarge,
+            ...styles.withHorizontalPadding,
+            width: '100%',
+            color: colors.black,
+          }}
+          onChangeText={txt => {
+            setQuery(txt.toLowerCase().trim());
+          }}
+          onSubmitEditing={searchMaterial}
+        />
       </View>
-    </SafeAreaView>
+      {results != null && (
+        <TextSubTitle text={t('searchResults', {count: results.length})} />
+      )}
+      <View
+        style={{
+          flexDirection: 'column',
+        }}>
+        {results != null &&
+          (results.length > 0 ? (
+            <ScrollView>
+              {results.map(item => {
+                return (
+                  <View key={item.code}>
+                    <DirectoryItem
+                      tipo="file"
+                      key={item.code}
+                      nome={item.nome}
+                      filename={item.filename}
+                      data_inserimento={item.data_inserimento}
+                      size_kb={item.size_kb}
+                      code={item.code}
+                      corso={item.corso}
+                    />
+                  </View>
+                );
+              })}
+              <View style={{marginBottom: 120}}></View>
+            </ScrollView>
+          ) : (
+            <View
+              style={{
+                flexDirection: 'column',
+                justifyContent: 'center',
+                alignItems: 'center',
+                width: '100%',
+                height: '100%',
+                marginTop: -styles.textExtraLarge.fontSize,
+              }}>
+              <Icon name="search-off" color={colors.gray} size={64} />
+              <TextS text={t('noResults')} weight="bold" color={colors.gray} />
+            </View>
+          ))}
+      </View>
+    </ScreenContainer>
   );
 }
