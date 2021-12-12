e088d38bcaa3c306b98241d93d036262f8d8b587
Robert Olaru <77898084+robertolaru@users.noreply.github.com>
2021-12-12 22:32:14 +0100

    ScreenContainer component

---
+++ src/components/ScreenContainer.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import {SafeAreaView, StatusBar, View} from 'react-native';
+import colors from '../colors';
+import styles from '../styles';
+
+export default function ScreenContainer({children}) {
+  return (
+    <SafeAreaView style={{height: '100%'}}>
+      <StatusBar
+        translucent
+        backgroundColor="transparent"
+        barStyle="dark-content"
+      />
+      <View
+        style={{
+          ...styles.container,
+          ...styles.safePaddingTop,
+          ...styles.withHorizontalPadding,
+          backgroundColor: colors.white,
+        }}>
+        {children}
+      </View>
+    </SafeAreaView>
+  );
+}
