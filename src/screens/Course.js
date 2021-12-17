d11822e2b79ca11bae76ba157e3a5fdc635dd537
Robert Olaru <77898084+robertolaru@users.noreply.github.com>
2021-12-17 16:49:40 +0100

    Course: info and videos tabs

--- src/screens/Course.js
+++ src/screens/Course.js
@@ -1,6 +1,6 @@
 import React, {useContext, useEffect, useState} from 'react';
 import {useTranslation} from 'react-i18next';
-import {View} from 'react-native';
+import {Pressable, View} from 'react-native';
 import {useDispatch} from 'react-redux';
 import colors from '../colors';
 import ArrowHeader from '../components/ArrowHeader';
@@ -9,6 +9,7 @@ import {TextL, TextN, TextS, TextXL} from '../components/Text';
 import {UserContext} from '../context/User';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 import styles from '../styles';
+import CourseInfo from '../components/CourseInfo';
 
 export default function Course({navigation, route}) {
   const {t} = useTranslation();
@@ -16,6 +17,19 @@ export default function Course({navigation, route}) {
   const {user} = useContext(UserContext);
   const [courseData, setCourseData] = useState(null);
   const code = route.params.courseCode;
+
+  const [currentTab, setCurrentTab] = useState('info');
+
+  const tabs = [
+    {
+      name: 'info',
+      icon: 'info-outline',
+    },
+    {
+      name: 'videos',
+      icon: 'ondemand-video',
+    },
+  ];
 
   useEffect(() => {
     if (!user.carico_didattico) {
@@ -39,34 +53,74 @@ export default function Course({navigation, route}) {
 
       {courseData && (
         <View>
-          <View>
-            <TextXL
-              text={courseData.nome}
-              numberOfLines={2}
-              weight="medium"
-              color={colors.black}
-            />
-            <View
-              style={{
-                flexDirection: 'row',
-                justifyContent: 'space-between',
-              }}>
-              <TextL text={code} color={colors.mediumGray} />
-              <View style={{flexDirection: 'column'}}>
-                <View
-                  style={{
-                    flexDirection: 'row',
-                    alignItems: 'center',
-                    justifyContent: 'flex-end',
-                    borderWidth: 1,
-                  }}>
-                  <Icon name="person-outline" color={colors.gray} size={16} />
-                  <TextS
-                    text={courseData.cognome_prof + ' ' + courseData.nome_prof}
-                  />
-                </View>
+          <TextXL
+            text={courseData.nome}
+            numberOfLines={2}
+            weight="medium"
+            color={colors.black}
+          />
+          <View
+            style={{
+              flexDirection: 'row',
+              justifyContent: 'space-between',
+            }}>
+            <TextL text={code} color={colors.mediumGray} />
+            <View style={{flexDirection: 'column'}}>
+              <View
+                style={{
+                  flexDirection: 'row',
+                  alignItems: 'center',
+                  justifyContent: 'flex-end',
+                  borderWidth: 1,
+                }}>
+                <Icon name="person-outline" color={colors.gray} size={16} />
+                <TextS
+                  text={courseData.cognome_prof + ' ' + courseData.nome_prof}
+                />
               </View>
             </View>
+          </View>
+          <View style={{flexDirection: 'row', justifyContent: 'space-around'}}>
+            {tabs.map(tab => (
+              <View
+                key={tab.name}
+                style={{
+                  flex: 1,
+                  flexDirection: 'row',
+                  justifyContent: 'center',
+                }}>
+                <Pressable
+                  android_ripple={{color: colors.lightGray}}
+                  onPress={() => {
+                    setCurrentTab(tab.name);
+                  }}
+                  style={{
+                    width: '100%',
+                    flexDirection: 'row',
+                    justifyContent: 'center',
+                  }}>
+                  <Icon
+                    name={tab.icon}
+                    size={32}
+                    color={
+                      tab.name == currentTab ? colors.gradient1 : colors.gray
+                    }
+                  />
+                </Pressable>
+              </View>
+            ))}
+          </View>
+
+          <View>
+            {(() => {
+              switch (currentTab) {
+                case 'info':
+                  return <CourseInfo />;
+                  break;
+                case 'videos':
+                  return <CourseVideos />;
+              }
+            })()}
           </View>
         </View>
       )}
