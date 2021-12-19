e1c5cfccab99a7f9a124ba36a1c7dc26dd5d943a
Robert Olaru <77898084+robertolaru@users.noreply.github.com>
2021-12-19 22:15:51 +0100

    Course loader

---
+++ src/components/CourseLoader.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import {Dimensions, View} from 'react-native';
+import {Rect} from 'react-native-svg';
+import SvgAnimatedLinearGradient from 'react-native-svg-animated-linear-gradient';
+import styles from '../styles';
+
+export default function CourseLoader() {
+  const w =
+    Dimensions.get('window').width -
+    2 * styles.withHorizontalPadding.paddingHorizontal;
+
+  const r = 8; // border radius
+
+  return (
+    <View style={{flexDirection: 'column'}}>
+      <SvgAnimatedLinearGradient height={200} width={w}>
+        <Rect x={0} y={0} rx={r} ry={r} width="190" height="36" />
+        <Rect x={0} y="48" rx={r} ry={r} width="128" height="24" />
+        <Rect x={0} y="90" rx={r} ry={r} width={w} height="64" />
+      </SvgAnimatedLinearGradient>
+    </View>
+  );
+}
