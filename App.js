802f468dcd11ed9903b37f1933d6daaddb13a751
Robert Olaru <77898084+robertolaru@users.noreply.github.com>
2021-12-20 12:12:41 +0100

    Set momentjs locale to app locale

--- App.js
+++ App.js
@@ -9,7 +9,10 @@ import EN from './src/locales/en';
 import IT from './src/locales/it';
 import * as RNLocalize from 'react-native-localize';
 
+import 'moment/locale/it';
+
 import {store} from './src/store/store';
+import moment from 'moment';
 
 let lng = '';
 if (RNLocalize.getLocales()[0].languageCode === 'it') {
@@ -28,6 +31,8 @@ i18n.use(initReactI18next).init({
   interpolation: {escapeValue: false},
   debug: false,
 });
+
+moment.locale(lng);
 
 export default function App() {
   useEffect(() => {
