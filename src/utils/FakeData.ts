664bf59705944914513f60d0248dab0ad8f8ffb0
Robert Olaru <77898084+robertolaru@users.noreply.github.com>
2022-01-17 16:00:51 +0100

    Setup jest and ts-jest; renamed files in src/utils; added FakeData methods and tests

---
+++ src/utils/FakeData.ts
@@ -0,0 +1,82 @@
+import faker from '@faker-js/faker';
+import {Cartella, File} from 'open-polito-api/corso';
+
+type DirectoryItem = File | Cartella;
+
+export default class FakeData {
+  /**
+   * Returns a File with random fields
+   */
+  static file(): File {
+    const name = faker.random.words(10);
+    return {
+      tipo: 'file',
+      code: faker.datatype.number(999999).toString(),
+      filename: name,
+      nome: name,
+      mime_type: '',
+      size_kb: faker.datatype.number(100000),
+      data_inserimento: faker.date.past(),
+    };
+  }
+
+  /**
+   * Returns an empty directory (Cartella) with random fields
+   */
+  static directory(): Cartella {
+    const name = faker.random.words(10);
+    return {
+      tipo: 'cartella',
+      code: faker.datatype.number(999999).toString(),
+      nome: name,
+      file: [],
+    };
+  }
+
+  /**
+   * Recursively generates a tree of File and Directory
+   * @param dirCount - The number of directories per level
+   * @param fileCount - The number of files per level
+   * @param levelCount - The number of levels in the tree (e.g. 1 gives a list with no nested items)
+   * @returns An array of DirectoryItem
+   */
+  static dirTree(
+    dirCount: number,
+    fileCount: number,
+    levelCount: number,
+  ): DirectoryItem[] {
+    const levelItems: DirectoryItem[] = [];
+
+    // If more levels needed, add nested directories by recursion, then add files
+    if (levelCount > 1) {
+      for (let i = 0; i < dirCount; i++) {
+        let dir = this.directory();
+        dir.file = this.dirTree(dirCount, fileCount, levelCount - 1);
+        levelItems.push(dir);
+      }
+    }
+
+    // If no more levels, only fill with files and return (base case)
+    for (let i = 0; i < fileCount; i++) {
+      levelItems.push(this.file());
+    }
+    return levelItems;
+  }
+
+  /**
+   * Returns a list of lists containing files and directories
+   */
+  static material(
+    numberOfCourses: number,
+    depthLevel: number,
+    dirsPerLevel: number,
+    filesPerDir: number,
+  ): any {
+    let material = [];
+    for (let i = 0; i < numberOfCourses; i++) {
+      let courseMaterial: DirectoryItem[] = [];
+
+      material.push(courseMaterial);
+    }
+  }
+}
