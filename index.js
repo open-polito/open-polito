1a6018c2f1650f6a9e36141171ca6a8343c98e25
Robert Olaru <77898084+robertolaru@users.noreply.github.com>
2022-03-06 22:38:23 +0100

    Fixed tracking because enums don't seem to work in js

--- index.js
+++ index.js
@@ -8,10 +8,7 @@ import App from './App';
 import {name as appName} from './app.json';
 import CodePush from 'react-native-code-push';
 import messaging from '@react-native-firebase/messaging';
-import {
-  NotificationType,
-  parsePushNotification,
-} from 'open-polito-api/notifications';
+import {parsePushNotification} from 'open-polito-api/notifications';
 
 const VARIANT = Config.VARIANT;
 const ENABLE_CODEPUSH = VARIANT != 'debug'; // disable CodePush in debug mode
@@ -27,7 +24,7 @@ if (Platform.OS == 'android' && VARIANT != 'debug') {
   messaging().setBackgroundMessageHandler(async remoteMessage => {
     const msg = parsePushNotification(remoteMessage);
     await Analytics.trackEvent('push_background', {
-      test: msg.topic == NotificationType.TEST ? 'true' : 'false',
+      test: msg.topic == 'test' ? 'true' : 'false',
     });
   });
 }
