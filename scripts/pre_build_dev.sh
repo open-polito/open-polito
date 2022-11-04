a39063747926dec83dbc2b3b77ed9763f8bd4291
Robert Olaru <77898084+robertolaru@users.noreply.github.com>
2022-11-04 22:30:48 +0100

    fix scripts

--- scripts/pre_build_dev.sh
+++ scripts/pre_build_dev.sh
@@ -2,5 +2,5 @@
 
 # Extract files from secrets
 echo $DEV_KEYSTORE | base64 -d > $GITHUB_WORKSPACE/dev-key-store.jks
-sed -i "s/APP_SECRET/$APP_SECRET/g" android/app/src/main/assets/appcenter-config.json
-echo $GOOGLE_SERVICES_JSON | base64 -d > $GITHUB_WORKSPACE/android/app/google-services.json
+sed -i "s/APP_SECRET/$APP_SECRET/g" packages/mobile/android/app/src/main/assets/appcenter-config.json
+echo $GOOGLE_SERVICES_JSON | base64 -d > $GITHUB_WORKSPACE/packages/mobile/android/app/google-services.json
