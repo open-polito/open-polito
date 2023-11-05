78b30d05b8ed160ff8d5cb78f56b10fd6a250eb7
Robert Olaru <77898084+robertolaru@users.noreply.github.com>
2023-11-05 23:41:22 +0100

    SecureStore: catch exceptions

--- lib/data/secure_store.dart
+++ lib/data/secure_store.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
 abstract class ISecureStore {
@@ -29,23 +30,29 @@ class SecureStore implements ISecureStore {
     return const SecureStore._(secureStore);
   }
 
-  @override
-  Future<String?> secureRead(SecureStoreKey key) async {
-    return await _secureStore.read(key: key.key);
+  Future<T?> _w<T>(Future<T?> Function() fn) async {
+    try {
+      return await fn();
+    } catch (e, s) {
+      if (kDebugMode) {
+        print("[SecureStore] Error: $e\nTrace: $s");
+      }
+    }
+    return null;
   }
 
   @override
-  Future<void> secureWrite(SecureStoreKey key, String? value) async {
-    await _secureStore.write(key: key.key, value: value);
-  }
+  Future<String?> secureRead(SecureStoreKey key) =>
+      _w(() => _secureStore.read(key: key.key));
 
   @override
-  Future<void> secureDelete(SecureStoreKey key) async {
-    await _secureStore.delete(key: key.key);
-  }
+  Future<void> secureWrite(SecureStoreKey key, String? value) =>
+      _w(() => _secureStore.write(key: key.key, value: value));
 
   @override
-  Future<void> clear() async {
-    await _secureStore.deleteAll();
-  }
+  Future<void> secureDelete(SecureStoreKey key) =>
+      _w(() => _secureStore.delete(key: key.key));
+
+  @override
+  Future<void> clear() => _w(() => _secureStore.deleteAll());
 }
