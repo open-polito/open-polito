4e771e9849271c65953e268c03c2923623adc3ed
Robert Olaru <77898084+robertolaru@users.noreply.github.com>
2023-11-16 18:59:20 +0100

    Basic sorting in Search screen

--- lib/data/data_repository/data_repository_search.dart
+++ lib/data/data_repository/data_repository_search.dart
@@ -14,6 +14,13 @@ extension DataRepositorySearch on DataRepository {
       demoDefault(
         real: () async {
           final processedQuery = query.toLowerCase().trim();
+
+          if (processedQuery == "") {
+            return null;
+          }
+
+          final defaultNow = DateTime.now();
+
           switch (category) {
             case SearchCategory.files:
               final files = <FileSearchResultItem>[];
@@ -27,11 +34,50 @@ extension DataRepositorySearch on DataRepository {
                 files.addAll(filesMatchingQuery.map(
                     (e) => FileSearchResultItem(courseId: course.id, file: e)));
               }
+
+              files.sort(
+                (a, b) {
+                  final firstTerm = sortOrder == SortOrder.desc ? b : a;
+                  final lastTerm = sortOrder == SortOrder.desc ? a : b;
+                  return switch (sortBy) {
+                    SortBy.createdAt => firstTerm.file.createdAt
+                        .compareTo(lastTerm.file.createdAt),
+                    SortBy.name =>
+                      firstTerm.file.name.compareTo(lastTerm.file.name),
+                    SortBy.size =>
+                      firstTerm.file.sizeKB.compareTo(lastTerm.file.sizeKB),
+                  };
+                },
+              );
+
               return SearchResult.files(files);
             case SearchCategory.people:
-              final people =
-                  (await req(() => api.getPeople(processedQuery)))?.data;
-              return SearchResult.people(people ?? []);
+              final people = (await req(() => api.getPeople(processedQuery)))
+                      ?.data
+                      .map((e) => e)
+                      .toList() // create a new list to avoid mutation
+                  ??
+                  [];
+
+              people.sort((a, b) {
+                final firstTerm = sortOrder == SortOrder.desc ? b : a;
+                final lastTerm = sortOrder == SortOrder.desc ? a : b;
+
+                // Comparison by name, with the last name having
+                // higher priority than the first name.
+
+                final cmp =
+                    firstTerm.lastName?.compareTo(lastTerm.lastName ?? "") ?? 0;
+                if (cmp == 0) {
+                  return firstTerm.firstName
+                          ?.compareTo(lastTerm.firstName ?? "") ??
+                      0;
+                }
+
+                return cmp;
+              });
+
+              return SearchResult.people(people);
             case SearchCategory.recordings:
               final recordings = <CourseVirtualClassroom>[];
               final courses = await getCourses() ?? [];
@@ -50,9 +96,25 @@ extension DataRepositorySearch on DataRepository {
                   }
                   return title.toLowerCase().contains(processedQuery);
                 });
-
                 recordings.addAll(courseVCs);
               }
+
+              recordings.sort(
+                (a, b) {
+                  final firstTerm = sortOrder == SortOrder.desc ? b : a;
+                  final lastTerm = sortOrder == SortOrder.desc ? a : b;
+                  return switch (sortBy) {
+                    SortBy.createdAt => firstTerm.recording?.createdAt
+                            ?.compareTo(
+                                lastTerm.recording?.createdAt ?? defaultNow) ??
+                        0,
+                    SortBy.name => firstTerm.recording?.title
+                            ?.compareTo(lastTerm.recording?.title ?? "") ??
+                        0,
+                    SortBy.size => 0,
+                  };
+                },
+              );
 
               return SearchResult.recordings(recordings);
           }
